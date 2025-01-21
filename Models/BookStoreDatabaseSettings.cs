1147d714a14a9a1f525c6955ea3d7244e1ea586e
Tinie-26 <nguyenngoctutrinh92@gmail.com>
2025-01-21 22:37:35 +0700

    Add project files.

---
+++ Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreAPI.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public required string ConnectionString { get; set; }
+        public required string DatabaseName { get; set; }
+        public required string BooksCollectionName { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using BookStoreAPI.Models;
+using BookStoreAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabaseSettings"));
+builder.Services.AddSingleton<BookService>();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
